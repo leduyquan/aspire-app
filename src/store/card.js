95a3b0df0bfce3dd20eae84052692167b147b529
Quan Duy <quanld.uit@gmail.com>
2022-03-09 11:57:47 +0700

    store

--- src/store/card.js
+++ src/store/card.js
@@ -1,65 +1,15 @@
-import CardService  from '../services/card.service';
-
 const state = {
     cards: [],
     status: ''
 }
 
 const mutations = {
-    getCardAllRequest(state) {
-        state.status = 'loading';
-    },
-    getCardAllSuccess(state, data) {
-        state.status = 'success';
-        state.cards = data;
-    },
-    getCardAllFailure(state) {
-        state.status = 'error';
-    },
-    onFreezeCardRequest() {
-        // state.cards =
-    },
-    onFreezeCardSuccess(state, data) {
-        state.cards = data
-    }
 };
 
 const actions = {
-    async getCardAll({ commit }) {
-        try {
-            commit('getCardAllRequest');
-            const response = await CardService.getCardAll();
-            commit('getCardAllSuccess', response.data);
-        } catch (error) {
-            commit('getCardAllFailure', error);
-        }
-    },
-    async onFreezeCardd({ commit }, data) {
-        try {
-            commit('onFreezeCardRequest');
-            if (data) {
-                console.log('haha')
-                const payload = { ...data };
-                payload.status = !data.status;
-                const response = await CardService.updateCard(payload.id, payload);
-                // if (response.statusText === "OK") {
-                //     cards[index]['status'] = payload.status;
-                //   this.cardData = a
-                //   console.log('cardData', this.cardData)
-                //   // this.getCardAll();
-                // }
-                commit('onFreezeCardSuccess', response.data);
-              }
-        } catch (error) {
-            // commit('getCardAllFailure', error);
-        }
-    },
-
 };
 
 const getters = {
-    cards: state => state.cards,
-    status: state => state.status,
 };
 
 export const card = {
